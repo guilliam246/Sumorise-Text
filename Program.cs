b614cec4d37177ed4c7fa7e09893039cceed6fa9
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-30 15:44:09 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sumorise_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+       
+        {
+            var sentence = "This is going to be a really really really really really really really long text";
+            var summary = StringUtility.SummoriseText(sentence, 25);
+            Console.WriteLine(summary);
+        }
+
+        
+    }
+}
